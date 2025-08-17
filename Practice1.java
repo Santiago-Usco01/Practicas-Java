84a283867c5d06b90fbb08fa58afa7545ae5b5a8
Santiago Cardenas Claros <u20251231614@usco.edu.co>
2025-08-17 01:08:16 -0500

    nada

--- Practice1.java
+++ Practice1.java
@@ -1,18 +1,21 @@
 package practice1;
 
+import java.util.Scanner;
+
 /**
  *
  * Santiago Cardenas Claros  u20251231614
  */
 public class Practice1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
+         Scanner sc= new Scanner(System.in);
         
+        System.out.println("Buen día, cuantos computadoras va a comprar?");
+      
         
+        int computersQuantity=sc.nextInt();
         
     }
     
