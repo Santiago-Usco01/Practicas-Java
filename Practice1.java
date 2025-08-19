afecd045a26422ad0021b330e100f7ff24c7ec50
Santiago Cardenas Claros <u20251231614@usco.edu.co>
2025-08-18 20:21:10 -0500

    Ejercicio 1 terminado

--- Practice1.java
+++ Practice1.java
@@ -9,13 +9,47 @@ import java.util.Scanner;
 public class Practice1 {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
         
-         Scanner sc= new Scanner(System.in);
+        double discount10 = 0.10;
+        double discount20 = 0.20;
+        double discount40 = 0.40;
         
-        System.out.println("Buen día, cuantos computadoras va a comprar?");
-      
+
+        System.out.println("Cuantos computadoras desea comprar? ");
+
+        int computersQuantity = sc.nextInt();
+   
+        int computerPrice = 500;
         
-        int computersQuantity=sc.nextInt();
+        int initialPrice = (computersQuantity*computerPrice);
+        
+        
+        
+        if (computersQuantity < 5) {
+            System.out.println("El valor de su compra es de: "+"U"+"$"+initialPrice);
+            System.out.println("Se le aplicara un descuento del 10% " );
+            double discountPrice10 = (initialPrice*discount10);
+            double finalPrice10 = (initialPrice-discountPrice10);
+            System.out.println("El valor final de su compra es de: "+"U"+"$"+finalPrice10);
+            
+    } else if (computersQuantity < 10) {
+        System.out.println("El valor de su compra es de: "+"U"+"$"+initialPrice);
+            System.out.println("Se le aplicará un descuento del 20% ");
+            double discountPrice20 = (initialPrice*discount20);
+            double finalPrice20 = (initialPrice-discountPrice20);
+            System.out.println("El valor final de su compra es de: "+"U"+"$"+finalPrice20);
+            
+
+        } else if (computersQuantity>=10){
+            System.out.println("El valor de su compra es de: "+"U"+"$"+initialPrice);
+            System.out.println("Se le aplicará un descuento del 40% ");
+            double discountPrice40 = (initialPrice*discount40);
+            double finalPrice40 = (initialPrice-discountPrice40);
+            System.out.println("El valor final de su compra es de: "+"U"+"$"+finalPrice40);
+    }
+        
         
     }
     
